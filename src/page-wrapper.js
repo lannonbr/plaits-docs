af411effa31499d5b48eeeb389c77194a64ecd0f
Benjamin Lannon <benjamin@lannonbr.com>
2020-08-02 11:27:46 -0400

    Finalize site

--- src/page-wrapper.js
+++ src/page-wrapper.js
@@ -4,19 +4,26 @@ import { Helmet } from "react-helmet";
 
 export default ({ children, ...props }) => {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <Helmet>
-        <link rel="stylesheet" href="style.css" />
+        <link rel="stylesheet" href="/style.css" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Inter&display=swa"
+        />
+        <title>Plaits Cheat Sheet</title>
       </Helmet>
-      <header className="bg-green-400 p-2">
-        <div className="max-w-6xl w-full mx-auto">
+      <header className="bg-green-400 p-2 mb-4">
+        <div className="max-w-6xl w-full mx-auto px-4">
           <h1>Plaits Cheat Sheet</h1>
         </div>
       </header>
-      <div className="max-w-6xl mx-auto px-2">{children}</div>
-      <footer>
-        The content on this website was provided by Mutable Instruments under a
-        cc-by-sa 3.0 license and is distributed under the same license.
+      <div className="flex-1 max-w-6xl w-full mx-auto px-4">{children}</div>
+      <footer className="bg-gray-300">
+        <p className="max-w-6xl mx-auto p-2 mb-0 text-center">
+          The content on this website was provided by Mutable Instruments under
+          a cc-by-sa 3.0 license and is distributed under the same license.
+        </p>
       </footer>
     </div>
   );
